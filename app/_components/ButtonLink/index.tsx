058f187ae351a44ce5a53932775332b7032f8b16
haruka0214 <ktc24a31e0015@edu.kyoto-tech.ac.jp>
2024-11-30 16:28:38 +0900

    こみっっと！

--- app/_components/ButtonLink/index.tsx
+++ app/_components/ButtonLink/index.tsx
@@ -1,31 +1,14 @@
-import Image from "next/image";
-
 import styles from "./index.module.css";
 
-type News={
-    id:string;
-    title:string;
-    category:{
-        name:string;
-    };
-    publishedAt:string;
-    createdAt:string;
-}
-
 type Props = {
-    news:News[];
+    href: string;
+    children: React.ReactNode;
 };
 
-export default function NewsList({ news }: Props) {
-    if (news.length === 0) {
-        return <div>記事がありません</div>;
-    }   
-    return (
-        <ul>
-            {news.map((article) => (
-                <li key={article.id}> className={styles.list}>
-                  <div classname={styles.link}>>
-                    
-                    
+export default function ButtonLink({ href, children }: Props) {
+    return(
+        <a href="{href}" className={styles.button}>
+            {children}
+        </a>
     );
 }
